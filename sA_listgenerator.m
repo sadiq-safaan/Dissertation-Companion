0a4b39aa2b4e4ad76b8cea27b78ab7b6ec95605a
sadiq-safaan <sadiq.safaan@gmail.com>
2025-04-26 15:04:42 -0400

    Update sA_listgenerator.m

--- sA_listgenerator.m
+++ sA_listgenerator.m
@@ -1,6 +1,14 @@
 function [sA] = sA_listgenerator(iter,n)
-%UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
+
+% function [sA] = sA_listgenerator(iter,n)
+% 
+% This function creates a cell array of DAG adjacency matrices.
+%
+% Inputs: iter = number of DAGs to be generated, n = size of DAG
+% Outputs: sA = cell array of DAG adjacency matrices
+%
+% Last Modified on 4/26/2025 by Safaan Sadiq
+
 sA={};
 for i=1:iter
     adj_mat = randi([0 1], n,n).*(ones(n)-eye(n));
