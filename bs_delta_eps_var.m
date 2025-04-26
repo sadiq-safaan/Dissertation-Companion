3ce1f27911ed6d702891f93b14bb571a2bab1b65
sadiq-safaan <sadiq.safaan@gmail.com>
2025-04-26 14:56:06 -0400

    Update bs_delta_eps_var.m

--- bs_delta_eps_var.m
+++ bs_delta_eps_var.m
@@ -1,33 +1,29 @@
-%adj_mat=[0 0 1; 0 0 0; 0 0 0];
-%adj_mat=[0 1 0 0 0;0 0 0 1 0;0 0 0 1 1; 0 0 0 0 0;0 0 0 0 0];
-%adj_mat= [0 0 0 1 0 1;0 0 1 0 0 1;0 0 0 1 0 0;0 0 0 0 1 1;0 0 0 0 0 1;0 0 0 0 0 0];
-iter=1000;
-n=8;
-sA=sA_listgenerator(iter,n);
-%adj_mat = randi([0 1], n,n).*(ones(n)-eye(n));
-%adj_mat = triu(adj_mat);
-b=ones(size(adj_mat,1),1);
-%n=size(adj_mat,1);
 
-delta_vec=0.1:0.1:3;
-att_vec=0*delta_vec;
+iter=1000; % Number of DAGs
+n=8; % Size of DAGs
+sA=sA_listgenerator(iter,n); % Generate cell array of DAGs
+b=ones(size(adj_mat,1),1); % Initialize drive vector
+
+delta_vec=0.1:0.1:3; % Initialize possible delta
 
 for i=1:length(delta_vec)
-    delta=delta_vec(i);
-    eps_vec=0.05:0.05:(delta/(1+delta));
+    delta=delta_vec(i); % Fix delta
+    eps_vec=0.05:0.05:(delta/(1+delta)); % Initialize possible epsilon
 
     for j=1:length(eps_vec)
-        eps=eps_vec(j);
+        eps=eps_vec(j); % Fix epsilon
 
-        count_tot=0;
-        count_acc=0;
+        count_tot=0; % Counts total number of conclusive predictions
+        count_acc=0; % Counts number of accurate predictions
 
         for k=1:iter
-            adj_mat=sA{k};
+            adj_mat=sA{k}; % Fix DAG
 
-            W=(-1-delta)*ones(n) + (1+delta)*eye(n) + (eps+delta)*adj_mat;
-            xbs=-inv(W)*b;
+            W=(-1-delta)*ones(n) + (1+delta)*eye(n) + (eps+delta)*adj_mat; % Initialize W matrix
+            xbs=-inv(W)*b; % Find balanced state
 
+            
+            % Numerical Simulation of balanced state trajectory
             soln = sA2soln(adj_mat,50,xbs,eps,delta,1);
             fp=soln.X(end,:);
             fp_supp=soln.X(end,:)>1e-4;
@@ -37,6 +33,7 @@ for i=1:length(delta_vec)
                 att_real=find(fp_supp>0);
             end
 
+            % Algorithm prediction
             att_predict=bsalgo(adj_mat);
 
             if att_predict~=0
@@ -47,7 +44,7 @@ for i=1:length(delta_vec)
                 count_acc=count_acc+1;
             end
         end
-        accuracy=count_acc/count_tot;
+        accuracy=count_acc/count_tot; % Compute accuracy by dividing correct predictions over total predictions
 
         figure(1)
         scatter(delta,eps,[],[accuracy, 0, 0],'filled')
